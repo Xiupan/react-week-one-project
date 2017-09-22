07d88a4539930da677c8776106305908096fd53a
Alan Hong <xiupan@gmail.com>
2017-09-22 14:11:45 -0500

    Bootstrap imported and hooked up.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import NavBar from './NavBar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar/>
+      </div>
+    );
+  }
+}
+
+export default App;
